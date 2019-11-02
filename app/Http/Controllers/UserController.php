3e531607ca71dc4386a0c3f807ad5ecd30eb4342
Koichi Taura <taura.koichi@gmail.com>
2019-11-02 14:27:15 +0900

    modify root and use controller to manage view

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: koichi.taura
+ * Date: 2019/11/02
+ * Time: 14:23
+ */
+
+namespace App\Http\Controllers;
+
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return view('welcome');
+    }
+}
