0bd608d85194cb00565975bc87db399b295a4d90
Taurin190 <taura.koichi@gmail.com>
2020-01-01 18:06:57 +0900

    add api function to read news with axios

--- resources/js/components/HackerNews.vue
+++ resources/js/components/HackerNews.vue
@@ -1,11 +1,54 @@
 <template>
 <div>
-    <h1>Hello World!!!!</h1>
+    <h1>Lastest Hacker News</h1>
+    <div>
+        <ul v-for="(news, index) in news_list" :key="index">
+            <li>
+                <h3>{{ news.title }}</h3>
+                <span>{{ news.score }} points by {{ news.by }}</span>
+                <span>comments</span>
+            </li>
+        </ul>
+    </div>
 </div>
 </template>
 <script>
+import axios from 'axios';
+
 export default {
   name: 'HackerNews',
-  mounted: function() {},
+  data: function(){
+      return {
+        news_id_list: [],
+        news_list: [],
+      }
+  },
+  mounted: function() {
+      this.loadLatestNews();
+  },
+  methods: {
+      loadLatestNews: function() {
+          console.log("loading...");
+          var response = axios.get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+          .then((res) => {
+              this.news_id_list = res.data;
+              this.news_id_list.forEach(
+                  (v, i) => {
+                      console.log(v);
+                      if (i > 20) return;
+                      this.news_list = [];
+                      this.getNewsDetailFromId(v);
+                  }
+              );
+          });
+      },
+      getNewsDetailFromId: function(id) {
+          console.log(id);
+          var detail = axios.get("https://hacker-news.firebaseio.com/v0/item/" + id + ".json?print=pretty")
+          .then((res) => {
+              this.news_list.push(res.data);
+          });
+      }
+  }
 }
 </script>
